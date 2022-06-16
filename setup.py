409c4dd4929ead8c008b9de62257f82175d20859
jordiabante <jordiabante@protonmail.com>
2022-06-16 10:09:43 -0700

    changed version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 HERE = pathlib.Path(__file__).parent
 
-VERSION = '0.0.1'
+VERSION = '0.1.0'
 PACKAGE_NAME = 'biodive'
 AUTHOR = 'Jordi Abante'
 AUTHOR_EMAIL = 'jordiabante@protonmail.com'
