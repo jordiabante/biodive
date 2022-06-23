95fe65072a2a969f1782b0aa6e285aa72f8c579d
jordiabante <jordiabante@protonmail.com>
2022-06-22 17:07:04 -0700

    updated version for pypi

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 HERE = pathlib.Path(__file__).parent
 
-VERSION = '0.1.1'
+VERSION = '0.1.2'
 PACKAGE_NAME = 'biodive'
 AUTHOR = 'Jordi Abante'
 AUTHOR_EMAIL = 'jordiabante@protonmail.com'
